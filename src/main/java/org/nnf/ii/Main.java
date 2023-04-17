0ccf79254caeb35f39c429e2239947033bd1033f
Nick07242000 <nicohidalgo2000@gmail.com>
2023-04-17 20:46:36 -0300

    [ADD] ImageRepository

--- src/main/java/org/nnf/ii/Main.java
+++ src/main/java/org/nnf/ii/Main.java
@@ -2,30 +2,16 @@ package org.nnf.ii;
 
 import org.nnf.ii.model.Image;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
-import java.util.UUID;
 
-import static org.nnf.ii.model.enums.Resolution.LOW;
-import static org.nnf.ii.model.enums.Size.SMALL;
-import static org.nnf.ii.model.enums.Status.READY;
+import static org.nnf.ii.repository.ImageRepository.findAll;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Image> images = new ArrayList<>();
-
-        while (images.size() != 100){
-            images.add(new Image(
-                    "https://s3.amazonaws.com/bucket/name-not-found/" + UUID.randomUUID(),
-                    SMALL,
-                    LOW,
-                    0,
-                    READY
-            ));
-        }
+        List<Image> images = findAll();
 
         Stack<Image> startContainer = new Stack<>();
 
